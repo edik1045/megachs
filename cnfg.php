12b6d1041c0c3e7eab81322c1480fd49f1924f67
edik1045 <66965109+edik1045@users.noreply.github.com>
2021-05-11 11:07:48 +0300

    Add files via upload

---
+++ cnfg.php
@@ -0,0 +1,12 @@
+<?php
+
+/*Configs By HACK-TO-WIN*/
+
+
+// Add your email //
+$info = 'https://megacash.me/server/backend/credits/setClientRewarded.php?email=remecedik@gmail.com&removeCredits=0&which=GPlay';
+
+?>
+
+
+
